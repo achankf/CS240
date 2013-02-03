1fb14edcc401e8a7bb81224ab1c6825c6a053717
Alfred Chan <alfred8844@gmail.com>
2013-02-03 18:09:07 -0500

    * updated q5

--- A2/cc/a02q5b.cpp
+++ A2/cc/a02q5b.cpp
@@ -1,8 +1,3 @@
-#if 0
-#include <iostream>
-using namespace std;
-#endif
-
 class min_heap{
 	int *array;
 	int array_sz, num_values;
@@ -79,22 +74,3 @@ void k_smallest( int* input_array, int num_input_values, int* output_array, int 
 		output_array[i] = heap.delete_min();
 	}
 }
-
-// driver
-#if 0
-int main(){
-	int num_input_values, k;
-	cin >> num_input_values;
-	cin >> k;
-	int input_array[num_input_values];
-	int output_array[num_input_values];
-	for (int i = 0; i < num_input_values; i++){
-		cin >> input_array[i];
-	}
-	k_smallest( input_array, num_input_values, output_array, k);
-	for (int i = 0; i < k; i++){
-		cout << output_array[i] << ' ';
-	}
-	cout << endl;
-}
-#endif
