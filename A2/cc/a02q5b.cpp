37a7e0ba08ded55aefe0c4b05e15acc826e3d7f0
Alfred Chan <alfred8844@gmail.com>
2013-02-04 19:03:34 -0500

    * updated a02q5b.cpp

--- A2/cc/a02q5b.cpp
+++ A2/cc/a02q5b.cpp
@@ -1,43 +1,20 @@
-class min_heap{
-	int *array;
-	int array_sz, num_values;
-
-private:
-	int left_child(int i);
-	
-public:
-	min_heap(int *array, int size);
-	int size();
-	void heapify();
-	int delete_min();
-	void bubble_down(int start);
-};
-
 void swap(int *array, int i, int j){
 	int temp = array[i];
 	array[i] = array[j];
 	array[j] = temp;
 }
 
-min_heap::min_heap(int *array, int size) :
-	array(array), array_sz(size), num_values(size)
-{}
-
-int min_heap::size(){
-	return num_values;
-}
-
-int min_heap::left_child(int i){
+int left_child(int i){
 	return 2*i + 1;
 }
 
-void min_heap::bubble_down(int start){
+void bubble_down(int *array, int *heap_size, int start){
 	int temp = array[start];
 	int i = start, compare;
 	while(true){
 		compare = left_child(i);
-		if (compare >= num_values) break;
-		if (compare + 1 < num_values && array[compare] > array[compare+1]){
+		if (compare >= *heap_size) break;
+		if (compare + 1 < *heap_size && array[compare] > array[compare+1]){
 			compare++;
 		}
 		if (temp < array[compare]) break;
@@ -46,31 +23,30 @@ void min_heap::bubble_down(int start){
 	}
 }
 
-int min_heap::delete_min(){
+int delete_min(int *array, int *heap_size){
 	int ret = array[0];
-	num_values--;
-	array[0] = array[num_values];
-	bubble_down(0);
+	*heap_size = *heap_size - 1;
+	array[0] = array[*heap_size];
+	bubble_down(array, heap_size, 0);
 	return ret;
 }
 
-void min_heap::heapify(){
-	for(int i = (num_values-1)/2; i >= 0; i--){
-		bubble_down(i);
+void heapify(int *array, int *heap_size){
+	for(int i = (*heap_size-1)/2; i >= 0; i--){
+		bubble_down(array, heap_size, i);
 	}
 }
 
 void k_smallest( int* input_array, int num_input_values, int* output_array, int k )
 {
-	int temp[num_input_values];
-
+	// copy input array into a temporary array -- should not contaminate input_array
+	int temp[num_input_values], heap_size = num_input_values;
 	for (int i = 0; i < num_input_values; i++){
 		temp[i] = input_array[i];
 	}
 
-	min_heap heap(temp, num_input_values);
-	heap.heapify();
+	heapify(temp, &heap_size);
 	for (int i = 0; i < k; i++){
-		output_array[i] = heap.delete_min();
+		output_array[i] = delete_min(temp, &heap_size);
 	}
 }
